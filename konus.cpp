a3ad526a3c8da28e2436983a0c24d7f3813935b1
UmidjonEsirgapov <61597929+UmidjonEsirgapov@users.noreply.github.com>
2020-03-15 21:02:25 +0500

    konus

---
+++ konus.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+      double h,r,v;
+      cin>>h>>r;
+      v=M_PI*r*r*h/3.0;
+      printf("%.2f",v);
+      return 0;
+    }
